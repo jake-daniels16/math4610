da0509bbbe06427a75ffd76ada9414c50372d4ee
jake_daniels16 <jake.daniels@usu.edu>
2022-09-14 15:25:36 -0600

    9/14

--- Root Finding Problem/Methods/fnIter.py
+++ Root Finding Problem/Methods/fnIter.py
@@ -19,13 +19,13 @@ def maxIter():
     mI = eval(input("Input max number of iterations here: "))
     return mI
 
-def g(f, x):
-    if x > 1:
-        gval = x + eval(f)
-        return gval
-    else:
-        gval = x - eval(f)
-        return gval
+def g1(f, x):
+    gval = x - eval(f)
+    return gval
+
+def g2(f, x):
+    gval = x + eval(f)
+    return gval
 
 def wantTable():
     print("Do you want a table?")
@@ -37,30 +37,45 @@ def wantTable():
 def table(a,b,c):
     print("(1)" , a, " (2) ", b, " (3) ", c)
 
-# Using g(x) = x - f(x) makes the derivative of g greater than one for all x > 1 for f(x) = x * e^-x
-# Using g(x) = x + f(x) makes the derivative of g greater than one for all x < 1 for above f(x)
-# Going to account for this by making an if else statement
-
 def functionalIteration():
     f = fn()
     x0 = initialGuess()
+    y0 = x0
     tol = tolerance()
     mI = maxIter()
     choice = wantTable()
     error = 10 * tol
     iter = 0
     if choice == 1:
+        print("Table for g1:")
         while error > tol and iter < mI:
-            x1 = g(f, x0)
+            x1 = g1(f, x0)
             error = np.abs(x1 - x0)
             x0 = x1
-            table(iter, x0, error)
+            table(iter + 1, x0, error)
+            iter = iter + 1
+        iter = 0
+        error = 10 * tol
+        print("Table for g2:")
+        while error > tol and iter < mI:
+            y1 = g2(f, y0)
+            error = np.abs(y1 - y0)
+            y0 = y1
+            table(iter + 1, y0, error)
             iter = iter + 1
     elif choice == 2:
         while error > tol and iter < mI:
-            x1 = g(f, x0)
+            x1 = g1(f, x0)
             error = np.abs(x1 - x0)
             x0 = x1
             iter = iter + 1
-        print("The root of f is at x = ", x0)
+        error = 10 * tol
+        iter = 0
+        while error > tol and iter < mI:
+            y1 = g2(f, y0)
+            error = np.abs(y1 - y0)
+            y0 = y1
+            iter = iter + 1
+        print("g1 approximates the root of f as x = ", x0)
+        print("g2 approximates the root of f as x = ", y0)
 
