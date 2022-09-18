3ff44249a91dee90e5cb1792c9cc8f1ae54bed74
jake_daniels16 <jake.daniels@usu.edu>
2022-09-18 09:24:09 -0600

    Added Newton's Method

---
+++ Root Finding Problem/Methods/newtons.py
@@ -0,0 +1,56 @@
+# Jake Daniels
+# MATH 4610
+
+import numpy as np
+
+def fn():
+    f = input("Enter function you want to find the roots for (in terms of x): ")
+    return f
+
+def fPrime():
+    fp = input("Enter derivative here: ")
+    return fp
+
+def f(fn, x):
+    fval = eval(fn)
+    return fval
+
+def initialGuess():
+    x0 = eval(input("Input initial guess for root here: "))
+    return x0
+
+def tolerance():
+    tol = eval(input("Input tolerance here: "))
+    return tol
+
+def maxIter():
+    mI = eval(input("Input max number of iterations here: "))
+    return mI
+
+def wantTable():
+    print("Do you want a table?")
+    print("\t1) Yes")
+    print("\t2) No")
+    choice = eval(input("Enter here: "))
+    return choice
+
+def table(a,b,c):
+    print("(1)" , a, " (2) ", b, " (3) ", c)
+
+def newtonsMethod():
+    f = fn()
+    fp = fPrime()
+    x0 = initialGuess()
+    tol = tolerance()
+    mI = maxIter()
+    # choice = wantTable()
+    error = 10 * tol
+    iter = 0
+    while error > tol and iter < mI:
+        f0 = eval(f, x0)
+        fp0 = eval(fp, x0)
+        x1 = x0 - f0 / fp0
+        error = np.abs(x1 - x0)
+        iter = iter + 1
+        x0 = x1
+    print("Root approximation: ", x0)
--- Root Finding Problem/Root Finding Problem.py
+++ Root Finding Problem/Root Finding Problem.py
@@ -3,13 +3,17 @@
 
 from Methods.fnIter import functionalIteration
 from Methods.bisect import bisectMethod
+from Methods.newtons import newtonsMethod
 
 print("Methods")
 print("\t1) Fixed Point Iteration")
 print("\t2) Bisect Method")
+print("\t3) Newton's Method")
 
 choice = eval(input("What method do you want to do? "))
 if choice == 1:
     functionalIteration()
 elif choice == 2:
     bisectMethod()
+elif choice == 3:
+    newtonsMethod()
