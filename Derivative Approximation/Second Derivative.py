d04c5510cbcc34571b7490926b168fcabfcb2dc5
jake_daniels16 <jake.daniels@usu.edu>
2022-10-10 12:20:18 -0600

    added loop

--- Derivative Approximation/Second Derivative.py
+++ Derivative Approximation/Second Derivative.py
@@ -5,12 +5,11 @@ import numpy as np
 
 def secondDerivative(f, x0, h):
     f0 = f(x0)
-    f1 = f(x0 + h)
-    f2 = f(x0 - h)
-    print("f0: ", f0)
-    print("f1: ", f1)
-    print("f2: ", f2)
-    fval = (f1 - 2 * f0 + f2) / (h * h)
+    for i in range (1, 20):
+        f1 = f(x0 + h)
+        f2 = f(x0 - h)
+        fval = (f1 - 2 * f0 + f2) / (h * h)
+        h = h / 2
     return fval
 
 approx = secondDerivative(lambda x: (x - np.pi / 2) * np.tan(x) * np.tan(x) / (x * x + 65), np.pi / 4, 1.0)
