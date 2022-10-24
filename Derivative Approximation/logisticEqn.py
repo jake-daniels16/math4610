7a2184395b0e0c5a05598ed4f3437db98c43ba1a
jake_daniels16 <jake.daniels@usu.edu>
2022-10-24 13:12:21 -0600

    beginning of task 5

--- Derivative Approximation/logisticEqn.py
+++ Derivative Approximation/logisticEqn.py
@@ -2,6 +2,7 @@
 # MATH 4610
 
 import numpy as np
+import matplotlib as plt
 from Methods.explicitEuler import explicitEuler
 from Methods.implicitEuler import implicitEuler
 
@@ -15,15 +16,15 @@ def logisticEquation(a, b, p0):
 tApprox1, xApprox1, tApprox2, xApprox2 = logisticEquation(0.2, 0.0005, 10.0)
 tApprox3, xApprox3, tApprox4, xApprox4 = logisticEquation(0.01, 0.0005, 10.0)
 tApprox5, xApprox5, tApprox6, xApprox6 = logisticEquation(2.0, 0.0005, 10.0)
-plt.plot(tApprox1, xApprox1, color='red')
+plt.plot(tApprox2, xApprox2, color='red')
 plt.xlabel('t')
 plt.ylabel('P(t)')
 plt.show()
-plt.plot(tApprox2, xApprox2, color='blue')
+plt.plot(tApprox4, xApprox4, color='blue')
 plt.xlabel('t')
 plt.ylabel('P(t)')
 plt.show()
-plt.plot(tApprox3, xApprox3, color='green')
+plt.plot(tApprox6, xApprox6, color='green')
 plt.xlabel('t')
 plt.ylabel('P(t)')
 plt.show()
